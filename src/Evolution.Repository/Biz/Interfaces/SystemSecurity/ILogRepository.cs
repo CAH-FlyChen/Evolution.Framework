0148e7b4d18181f0bcb5b29513ad4f3b12f63bb8
GilChen <chenflyxp@gmail.com>
2016-09-26 09:59:41 +0800

    批量替换授权title

--- src/Evolution.Repository/Biz/Interfaces/SystemSecurity/ILogRepository.cs
+++ src/Evolution.Repository/Biz/Interfaces/SystemSecurity/ILogRepository.cs
@@ -2,7 +2,7 @@
  * Copyright © 2016 Evolution.Framework 版权所有
  * Author: Evolution
  * Description: Evolution快速开发平台
- * Website：http://www.nfine.cn
+ * Website：
 *********************************************************************************/
 using Evolution.IRepository;
 using Evolution.Domain.Entity.SystemSecurity;
