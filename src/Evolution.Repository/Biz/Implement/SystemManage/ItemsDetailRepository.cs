0d207049159ef02d96cabf23b19d1e9aa4316fcc
GilChen <chenflyxp@gmail.com>
2016-09-27 15:18:01 +0800

    将字典的sql改为lambda

--- src/Evolution.Repository/Biz/Implement/SystemManage/ItemsDetailRepository.cs
+++ src/Evolution.Repository/Biz/Implement/SystemManage/ItemsDetailRepository.cs
@@ -13,31 +13,41 @@ using System.Collections.Generic;
 using System.Data.Common;
 using System.Data.SqlClient;
 using System.Text;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace Evolution.Repository.SystemManage
 {
     public class ItemsDetailRepository : RepositoryBase<ItemsDetailEntity>, IItemsDetailRepository
     {
+        private EvolutionDbContext ctx = null;
         public ItemsDetailRepository(EvolutionDbContext ctx) : base(ctx)
         {
-
+            this.ctx = ctx;
         }
         public List<ItemsDetailEntity> GetItemList(string enCode)
         {
-            StringBuilder strSql = new StringBuilder();
-            strSql.Append(@"SELECT  d.*
-                            FROM    Sys_ItemsDetail d
-                                    INNER  JOIN Sys_Items i ON i.F_Id = d.F_ItemId
-                            WHERE   1 = 1
-                                    AND i.F_EnCode = @enCode
-                                    AND d.F_EnabledMark = 1
-                                    AND d.F_DeleteMark = 0
-                            ORDER BY d.F_SortCode ASC");
-            DbParameter[] parameter = 
-            {
-                 new SqlParameter("@enCode",enCode)
-            };
-            return this.FindList(strSql.ToString(), parameter);
+            var x = from a in ctx.ItemsDetails
+                     join b in ctx.Items on a.ItemId equals b.Id
+                     where b.EnCode == enCode && a.EnabledMark == true && a.DeleteMark == false
+                     orderby a.SortCode
+                     select a;
+            return x.ToList();
+            
+            //StringBuilder strSql = new StringBuilder();
+            //strSql.Append(@"SELECT  d.*
+            //                FROM    Sys_ItemsDetail d
+            //                        INNER  JOIN Sys_Items i ON i.F_Id = d.F_ItemId
+            //                WHERE   1 = 1
+            //                        AND i.F_EnCode = @enCode
+            //                        AND d.F_EnabledMark = 1
+            //                        AND d.F_DeleteMark = 0
+            //                ORDER BY d.F_SortCode ASC");
+            //DbParameter[] parameter = 
+            //{
+            //     new SqlParameter("@enCode",enCode)
+            //};
+            //return this.FindList(strSql.ToString(), parameter);
         }
     }
 }
